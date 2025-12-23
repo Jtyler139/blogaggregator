6826ccbbba41b865c450cb3e4110d9ed07600346
Jtyler139 <jtyler0226@gmail.com>
2025-12-23 15:25:25 -0600

    updated internal/config to match solution files

--- internal/config/config.go
+++ internal/config/config.go
@@ -3,63 +3,71 @@ package config
 import (
 	"encoding/json"
 	"os"
+	"path/filepath"
 )
 
 const configFileName = ".gatorconfig.json"
 
 type Config struct {
-	DataBaseName 	string `json:"db_url"`
+	DBURL			string `json:"db_url"`
 	CurrentUserName string `json:"current_user_name"`
 }
 
+func (cfg *Config) SetUser(userName string) error {
+	cfg.CurrentUserName = userName
+	return write(*cfg)
+}
+
 func Read() (Config, error) {
+	fullPath, err := getConfigFilePath()
+	if err != nil {
+		return Config{}, err
+	}
+
+	file, err := os.Open(fullPath)
+	if err != nil {
+		return Config{}, err
+	}
+	defer file.Close()
+
+	decoder := json.NewDecoder(file)
 	cfg := Config{}
-	fileLocation, err := getConfigFilePath()
+	err = decoder.Decode(&cfg)
 	if err != nil {
-		return cfg, err
+		return Config{}, err
 	}
-	data, err := os.ReadFile(fileLocation)
-	if err != nil {
-		return cfg, err
-	}
-	jsonData := []byte(data)
 	
-	err = json.Unmarshal(jsonData, &cfg)
-	if err != nil {
-		return cfg, err
-	}
 	return cfg, nil
 }
 
-func (cfg *Config) SetUser(userName string) {
-	cfg.CurrentUserName = userName
-	err := write(cfg)
-	if err != nil {
-		return
-	}
-}
+
 
 func getConfigFilePath() (string, error) {
-	fullPath, err := os.UserHomeDir()
+	home, err := os.UserHomeDir()
 	if err != nil {
 		return "", err
 	}
-	fullPath = fullPath + "/.gatorconfig.json"
+	fullPath := filepath.Join(home, configFileName)
 	return fullPath, nil
 }
 
-func write(cfg *Config) error {
+func write(cfg Config) error {
 	fullPath, err := getConfigFilePath()
 	if err != nil {
 		return err
 	}
-	jsonData, err := json.Marshal(cfg)
+	
+	file, err := os.Create(fullPath)
 	if err != nil {
 		return err
 	}
-	err = os.WriteFile(fullPath, jsonData, 0644)
+	defer file.Close()
+
+	encoder := json.NewEncoder(file)
+	err = encoder.Encode(cfg)
 	if err != nil {
 		return err
 	}
+
 	return nil
 }
--- main.go
+++ main.go
@@ -3,19 +3,24 @@ package main
 import (
 	"github.com/jtyler139/blogaggregator/internal/config"
 	"fmt"
+	"log"
 )
 
 func main() {
-	cfg := config.Config{}
 	cfg, err := config.Read()
 	if err != nil {
-		fmt.Println(err)
+		log.Fatalf("error reading config: %v", err)
 	}
-	fmt.Println(cfg)
-	cfg.SetUser("Tyler")
+	fmt.Printf("Read config : %v\n", cfg)
+
+	err = cfg.SetUser("Tyler")
+	if err != nil {
+		log.Fatalf("couldn't set current user: %v", err)
+	}
+
 	cfg, err = config.Read()
 	if err != nil {
-		fmt.Println(err)
+		log.Fatalf("error reading config: %v", err)
 	}
-	fmt.Println(cfg)
+	fmt.Printf("Read config again: %v\n", cfg)
 }
