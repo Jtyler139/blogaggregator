3bd20a81909bc4e2355532ce8545098c0917c6df
Jtyler139 <jtyler0226@gmail.com>
2025-12-28 10:37:47 -0600

    added Register function and database access

--- handler_user.go
+++ handler_user.go
@@ -2,6 +2,11 @@ package main
 
 import (
 	"fmt"
+	"github.com/google/uuid"
+	"context"
+	"time"
+	"os"
+	"github.com/jtyler139/blogaggregator/internal/database"
 )
 
 func handlerLogin(s *state, cmd command) error {
@@ -9,12 +14,54 @@ func handlerLogin(s *state, cmd command) error {
 		return fmt.Errorf("usage: %s <name>", cmd.Name)
 	}
 	name := cmd.Args[0]
+	ctx := context.Background()
 
-	err := s.cfg.SetUser(name)
+	_, err := s.db.GetUser(ctx, name)
+	if err != nil {
+		fmt.Printf("User <%s> does not exist\n", name)
+		os.Exit(1)
+	}
+
+	err = s.cfg.SetUser(name)
 	if err != nil {
 		return fmt.Errorf("couldn't set current user: %w", err)
 	}
 
 	fmt.Println("User switched successfully!")
+	return nil
+}
+
+func handlerRegister(s *state, cmd command) error {
+	if len(cmd.Args) != 1 {
+		return fmt.Errorf("usage: %s <name>", cmd.Name)
+	}
+	name := cmd.Args[0]
+	ctx := context.Background()
+
+	u, err := s.db.GetUser(ctx, name)
+	if err == nil {
+		fmt.Printf("user <%s> already exists\n", u.Name)
+		os.Exit(1)
+	}
+
+
+	user, err := s.db.CreateUser(ctx, database.CreateUserParams{
+		ID: 		uuid.New(),
+		CreatedAt: 	time.Now(),
+		UpdatedAt: 	time.Now(),
+		Name: 		name,
+	})
+	if err != nil {
+		return fmt.Errorf("error registering new user: %v", err)
+	}
+
+	err = s.cfg.SetUser(name)
+	if err != nil {
+		return fmt.Errorf("couldn't set current user: %w", err)
+	}
+
+
+	fmt.Printf("New user %s was created\n", user.Name)
+
 	return nil
 }
--- main.go
+++ main.go
@@ -1,22 +1,37 @@
 package main
 
+import _ "github.com/lib/pq"
+
 import (
 	"github.com/jtyler139/blogaggregator/internal/config"
 	"os"
 	"log"
+	"database/sql"
+	"github.com/jtyler139/blogaggregator/internal/database"
 )
 
 type state struct {
+	db	*database.Queries
 	cfg *config.Config
 }
 
 func main() {
+	dbURL := "postgres://postgres:postgres@localhost:5432/gator"
+
+	db, err := sql.Open("postgres", dbURL)
+	if err != nil {
+		log.Fatalf("error opening database: %v", err)
+	}
+
+	dbQueries := database.New(db)
+
 	cfg, err := config.Read()
 	if err != nil {
 		log.Fatalf("error reading config: %v", err)
 	}
 	
 	programState := &state{
+		db: dbQueries,
 		cfg: &cfg,
 	}
 
@@ -24,6 +39,7 @@ func main() {
 		registeredCommands: make(map[string]func(*state, command) error),
 	}
 	cmds.register("login", handlerLogin)
+	cmds.register("register", handlerRegister)
 
 	if len(os.Args) < 2 {
 		log.Fatal("Usage: cli <command> [args...]")
