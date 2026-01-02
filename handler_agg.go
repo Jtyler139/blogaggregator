162659b654cd323de27517b791992ff0420e1e7d
Jtyler139 <jtyler0226@gmail.com>
2026-01-02 16:05:49 -0600

    added scrape feeds function

--- handler_agg.go
+++ handler_agg.go
@@ -3,14 +3,48 @@ package main
 import (
 	"fmt"
 	"context"
+	"time"
 )
 
 func handlerAgg(s *state, cmd command) error {
-	feed, err := fetchFeed(context.Background(), "https://www.wagslane.dev/index.xml")
+	if len(cmd.Args) != 1 {
+		return fmt.Errorf("usage: %s <time between reqs>", cmd.Name)
+	}
+
+	timeBetweenReqs, err := time.ParseDuration(cmd.Args[0])
+	if err != nil {
+		return fmt.Errorf("couldn't parse time: %w", err)
+	}
+
+	fmt.Printf("Collecting feeds every %v\n", timeBetweenReqs)
+
+	ticker := time.NewTicker(timeBetweenReqs)
+	for ; ; <-ticker.C {
+		scrapeFeeds(s)
+	}
+
+	return nil
+}
+
+func scrapeFeeds(s *state) error {
+	nextFeed, err := s.db.GetNextFeedToFetch(context.Background())
+	if err != nil {
+		return fmt.Errorf("couldn't get next feed: %w", err)
+	}
+
+	markFeed, err := s.db.MarkFeedFetched(context.Background(), nextFeed.ID)
+	if err != nil {
+		return fmt.Errorf("couldn't mark next feed: %w", err)
+	}
+
+	feed, err := fetchFeed(context.Background(), markFeed.Url)
 	if err != nil {
 		return fmt.Errorf("couldn't fetch feed: %w", err)
 	}
 
-	fmt.Printf("Feed: %+v\n", feed)
+	for _, item := range feed.Channel.Item {
+		fmt.Printf("Feed: %s\n", item.Title)
+	}
+
 	return nil
 }
